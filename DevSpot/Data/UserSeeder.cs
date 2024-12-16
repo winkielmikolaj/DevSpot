6e6de6f2bd09b51dfa544d46143017036322f576
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-12-16 18:37:04 +0100

    Writing a code to seed a new user

---
+++ DevSpot/Data/UserSeeder.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using DevSpot.Constants;
+
+namespace DevSpot.Data
+{
+    public class UserSeeder
+    {
+        public static async Task SeedUsersAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            if (await userManager.FindByEmailAsync("admin@devspot.com") == null)
+            {
+                var user = new IdentityUser
+                {
+                    Email = "admin@devspot.com",
+                    EmailConfirmed = true,
+                    UserName = "admin@devspot.com"
+                };
+
+                //Admin123! is password
+                var result = await userManager.CreateAsync(user, "Admin123!");
+
+                if (result.Succeeded)
+                {
+                    //przypisywanie roli jesli dodanie sie powiedzie
+                    await userManager.AddToRoleAsync(user, Roles.Admin);
+                }
+            }
+        }
+    }
+}
