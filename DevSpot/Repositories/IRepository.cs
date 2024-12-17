c7301b888fd8d4b74412570d2492f0791ee7588f
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2024-12-17 13:26:26 +0100

    Created a generic interface

---
+++ DevSpot/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace DevSpot.Repositories
+{
+    public interface IRepository<T> where T : class //where T : class to jest constraint
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(int id);
+
+        //T entity = job postings
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(int id);
+    }
+}
