7c132a7c5f7f99e9f9e29e9c86b4efd48160ca00
Mikołaj 140635 <pzx112240@student.poznan.merito.pl>
2025-01-02 18:27:38 +0100

    Adjusted Unit Test project

---
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,28 @@
+﻿using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevSpot.Tests
+{
+    internal class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("JobPostingDb")
+                .Options;
+        }
+
+        private ApplicationDbContext CreateDbContext()
+        {
+            return new ApplicationDbContext(_options);
+        }
+    }
+}
